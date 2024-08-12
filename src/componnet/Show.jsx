7dd0aff8e3aba0fb3e4b71fa38525bb17fd910ee
adity tripathi <tripathiaditya163@gmail.com>
2024-08-12 23:53:31 +0530

    Change UI in Show page

--- src/componnet/Show.jsx
+++ src/componnet/Show.jsx
@@ -15,7 +15,7 @@ const Show = (props) => {
       <p className='text-4xl font-bold mx-[37%] mt-16 text-[#EA580C]'>No Pending Task</p>
       <div className="alltaskas flex w-96 mx-auto flex-col gap-4">
         {
-          data.map((task, index) => (<div className="task w-full h-[40px] bg-gray-400 mx-auto ">{task.taskname}
+          sdata.map((task, index) => (<div className="task w-full h-[40px] bg-gray-400 mx-auto ">{task.taskname}
           </div>))
         }
       </div>
